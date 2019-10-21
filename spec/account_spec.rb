c81c87b4116acb21f4eb750c25304f9b6c712b53
Thomas Ross <thomasross3610@gmail.com>
2019-10-21 11:16:38 +0100

    #deposit method added to add value to account balance.  Tests green

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,15 @@
 require 'account'
 
+account = Account.new
+
 describe Account do
   it 'Is initialized with a balance of 0' do
-    account = Account.new
     expect(account.balance).to eq 0
+  end
+
+  describe '#deposit' do
+    it 'increases the balance by the amount deposited' do
+      expect{account.deposit(10)}.to change{account.balance}.from(0).to(10)
+    end
   end
 end
